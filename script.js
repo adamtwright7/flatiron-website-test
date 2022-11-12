d57ea51f5a524c6f3abd539a290a3f00725fafcb
Adam Wright <adamtwright7@gmail.com>
2022-11-12 18:30:54 -0500

    ok I'm done now

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Nathair procures only the best Pis for Her Majesty Titania, the Seelie Queen";
 document.querySelector("body").appendChild(h2);
